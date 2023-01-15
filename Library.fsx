ac97356d2811692f6ad3e16ef8070cd11abdb56d
Vlad Dyshakov <kawaii.nekotech@gmail.com>
2023-01-16 01:50:52 +0700

    Initial commit

---
+++ Library.fsx
@@ -0,0 +1,18 @@
+﻿#r "nuget: Giraffe.ViewEngine, 1.4.0"
+open Giraffe.ViewEngine
+
+#r "nuget: System.Text.Json"
+open System.Text.Json
+
+let _ = 1 |> JsonSerializer.Serialize
+
+let export() =
+    html [] [
+        head [] [
+        ]
+        body [] [
+            header [] [
+                str "Saasdassasdsadsadasadasdasmple page"
+            ]
+        ]
+    ]
